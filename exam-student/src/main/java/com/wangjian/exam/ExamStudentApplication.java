888fb5230c5908dcd80d5e0a66302b28eaaa7bed
“wangjian” <wangjian_hf@163.com>
2022-08-10 16:46:47 +0800

    优化

---
+++ exam-student/src/main/java/com/wangjian/exam/ExamStudentApplication.java
@@ -0,0 +1,13 @@
+package com.wangjian.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamStudentApplication.class, args);
+    }
+
+}
